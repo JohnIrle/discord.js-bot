a1190581d6f67c164d55e48c5b4772092b2278ab
JohnIrle <irlejohn@gmail.com>
2020-08-26 22:05:53 -0500

    Finish reactions

--- src/bot.js
+++ src/bot.js
@@ -2,14 +2,61 @@ require("dotenv").config();
 
 const { Client } = require("discord.js");
 
-const bot = new Client();
+const bot = new Client({
+    partials: ["MESSAGE", "REACTION"],
+});
+const PREFIX = "$";
 
 bot.on("ready", () => {
     console.log(`${bot.user.tag} has logged in`);
 });
 
 bot.on("message", (message) => {
-    console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+    }
+});
+
+bot.on("messageReactionAdd", (reaction, user) => {
+    const { name } = reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === "748369219464200197") {
+        switch (name) {
+            case "🍎":
+                member.roles.add("747563550813323388");
+                break;
+            case "🍌":
+                member.roles.add("748372147621068913");
+                break;
+            case "🍇":
+                member.roles.add("748372145050222593");
+                break;
+            case "🍑":
+                member.roles.add("748372142885699684");
+                break;
+        }
+    }
+});
+
+bot.on("messageReactionRemove", (reaction, user) => {
+    const { name } = reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === "748369219464200197") {
+        switch (name) {
+            case "🍎":
+                member.roles.remove("747563550813323388");
+                break;
+            case "🍌":
+                member.roles.remove("748372147621068913");
+                break;
+            case "🍇":
+                member.roles.remove("748372145050222593");
+                break;
+            case "🍑":
+                member.roles.remove("748372142885699684");
+                break;
+        }
+    }
 });
 
 bot.login(process.env.DISCORDJS_BOT_TOKEN);
